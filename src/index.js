6baa4a8a93c42b93f233c676ed2db461ced085b7
Dalton Knuckles <djck244@gmail.com>
2023-04-14 13:40:59 -0700

    complete mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      let task = document.querySelector('#new-task-description').value
+      buildToDo(task);
+      form.reset();
+    })
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  let edt = document.createElement('button');
+  edt.addEventListener('click', handleEdit);
+  btn.addEventListener('click', handleDelete);
+  edt.style.fontSize = '7px'
+  edt.textContent = 'Edit'
+  btn.style.fontSize = '10px'
+  btn.style.fontWeight = 'bold'
+  btn.textContent = 'x '
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  li.appendChild(edt)
+  document.querySelector('#tasks').appendChild(li);
+};
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+function handleEdit(e) {
+  const listItem = e.target.parentNode;
+  const oldText = listItem.firstChild.textContent;
+  const newText = prompt("Enter new todo text:", oldText);
+
+  if (newText !== null) {
+    listItem.firstChild.textContent = newText;
+  }};
